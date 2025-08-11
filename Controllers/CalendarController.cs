c24d085f430b09a7dda0b685bef8b96962bb2b14
RadoslawG123 <gorecki208@onet.pl>
2025-08-11 16:50:36 +0200

    Create ViewModel: CalendarEventViewModel and also CallendarController and index.cshtml in view folder | To index.cshtml added simple FullCalendar

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgressTrackerApp.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/CalendarEventViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ProgressTrackerApp.Models
+{
+    public class CalendarEventViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime Start { get; set; }
+        public DateTime? End { get; set; }
+        public string Color { get; set; } = null!;
+        public string Type { get; set; } = null!;
+
+        public CalendarEventViewModel()
+        {
+            
+        }
+    }
+}
